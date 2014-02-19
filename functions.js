8c31b38eff7101b182558e184478ed17da8fb696
Wilco Waaijer <w_waaijer@hotmail.com>
2014-02-19 13:28:16 +0100

    functions.js
    Basic script for makeing it possible to run the same code on different
    browsers

---
+++ functions.js
@@ -0,0 +1,23 @@
+addListener = typeof window.addEventListener !== "undefined" ? (
+        function(element, event, fn) {
+            element.addEventListener( event, fn );
+        }
+) : (
+        function(element, event, fn) {
+            element.attachEvent( "on" + event, fn );
+        }
+);
+
+removeListener = typeof window.removeEventListener !== "undefined" ? (
+        function(element, event, fn) {
+            element.removeEventListener( event, fn );
+        }
+) : (
+        function(element, event, fn) {
+            element.detachEvent( "on" + event, fn );
+        }
+);
+
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
